062a60d8850b82e073dcc01c092bf62b17aa6280
Patrick Hallinan <patrick.j.hallinan@gmail.com>
2024-12-11 11:03:33 -0500

    add bz2 for ubuntu so we can merge to master

--- script/rocky-linux-9-deps.sh
+++ script/rocky-linux-9-deps.sh
@@ -4,7 +4,7 @@ sudo dnf update -y
 sudo dnf config-manager --set-enabled crb
 sudo dnf install -y cmake gcc gcc-c++ boost boost-devel double-conversion-devel
 sudo dnf install -y fast_float-devel libevent-devel openssl-devel glog-devel zlib-devel libsodium-devel gperf
-sudo dnf install libunwind-devel binutils-devel xz-devel lz4-devel snappy-devel libzstd-devel
+sudo dnf install libunwind-devel binutils-devel xz-devel lz4-devel snappy-devel libzstd-devel bzip2-devel
 
 
 # fmt
