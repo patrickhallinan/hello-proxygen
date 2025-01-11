b7b23311e4fae779cd1dcac9b3f06edf623b59e6
Patrick <patrick@syn-2603-7081-20f0-7100-0000-0000-0000-1252.res6.spectrum.com>
2025-01-11 13:30:33 -0500

    wip

--- README.md
+++ README.md
@@ -2,12 +2,25 @@
   
 Tested on ubuntu 24.04 and Rockly Linux 9 (red hat clone)
 
+## install dependencies
+
+### Rocky Linux 9 (red hat clone)
+
+----
+script/rocky-linux-9-deps.sh
+----
+
 ## get repo
 ```
 git clone --recurse-submodules  git@github.com:patrickhallinan/hello-proxygen
 ```
 
 ## build proxygen
+
+----
+c++: fatal error: Killed signal terminated program cc1plus
+compilation terminated.
+----
 
 **From project root**
 
--- script/rocky-linux-9-deps.sh
+++ script/rocky-linux-9-deps.sh
@@ -4,7 +4,8 @@ sudo dnf update -y
 sudo dnf config-manager --set-enabled crb
 sudo dnf install -y cmake gcc gcc-c++ boost boost-devel double-conversion-devel
 sudo dnf install -y fast_float-devel libevent-devel openssl-devel glog-devel zlib-devel libsodium-devel gperf
-sudo dnf install libunwind-devel binutils-devel xz-devel lz4-devel snappy-devel libzstd-devel bzip2-devel
+sudo dnf install -y libunwind-devel binutils-devel xz-devel lz4-devel snappy-devel libzstd-devel bzip2-devel
+sudo dnf install -y clang fmt-devel
 
 
 # fmt
