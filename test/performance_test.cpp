3e6089eb32ec2b57954b6f6a694ead59ccca5785
patrick <patrick.j.hallinan@gmail.com>
2025-01-28 14:04:38 -0500

    wip

--- test/performance_test.cpp
+++ test/performance_test.cpp
@@ -2,6 +2,7 @@
 
 #include "HttpClient.h"
 
+#include <folly/futures/ThreadWheelTimekeeper.h>
 #include <folly/init/Init.h>
 #include <folly/io/async/EventBase.h>
 #include <gflags/gflags.h>
@@ -15,7 +16,7 @@ DEFINE_string(target_host, "127.0.0.1", "IP address");
 DEFINE_int32(target_port, 8080, "HTTP port");
 
 DEFINE_int32(num_connections, 16, "should be a power of 2");
-DEFINE_int32(number_of_requests, 1000, "number of http requests");
+DEFINE_int32(number_of_requests, 100000, "number of http requests");
 DEFINE_int32(payload_size, 2000, "number of bytes to send in each request");
 
 void performance_test(folly::EventBase*);
@@ -42,37 +43,8 @@ std::atomic<int> requestCount(0);
 // Bit rate
 
 
-/*
-static void nacho(folly::EventBase* eb) {
-    using namespace folly;
-
-    eb->runInEventBaseThread([eb]() {
-        auto p = std::make_shared<Promise<folly::Unit>>();
-
-        LOG(INFO) << "NACHO";
-
-        auto defaultTimeout = std::chrono::milliseconds(500);
-        proxygen::WheelTimerInstance timer{defaultTimeout, eb};
-
-        return p->getFuture()
-            //.within(std::chrono::milliseconds(500), &eb->timer())
-            //.within(std::chrono::milliseconds(500), std::runtime_error("Salsa"))
-            .within(std::chrono::milliseconds(500), timer, std::runtime_error("Salsa"))
-            .thenValue([](folly::Unit) {
-                LOG(INFO) << ".thenValue()";
-            })
-            //.thenError(folly::tag_t<folly::OperationCancelled>{}, [p](const folly::OperationCancelled& e) {
-            .thenError(folly::tag_t<std::exception>{}, [p](const std::exception& e) {
-                LOG(INFO) << ".thenError()  TACO";
-                LOG(INFO) << e.what();
-                p->setValue(folly::unit);
-            });
-    });
-}
-*/
-
-
 int main(int argc, char* argv[]) {
+
     FLAGS_logtostderr = true;
     FLAGS_minloglevel = 0;
 
@@ -81,11 +53,9 @@ int main(int argc, char* argv[]) {
     folly::EventBase eventBase;
 
     performance_test(&eventBase);
-    //nacho(&eventBase);
 
     // test does not run until eventBase.loop()
     eventBase.loop();
-    //eventBase.loopForever();
 
     LOG(INFO) << "Exit main()\n";
 
@@ -94,19 +64,10 @@ int main(int argc, char* argv[]) {
 
 
 folly::Future<HttpClient*> send(HttpClient* client, std::string& payload) {
-    if (client == nullptr) {
-        LOG(INFO) << "Nacho";
-    }
-
     return client->POST(payload)
         .thenValue([client, &payload](const HttpResponse& response) -> folly::Future<HttpClient*> {
             if (++requestCount < FLAGS_number_of_requests)
                 return send(client, payload);
-            return folly::makeFuture(client);
-        })
-        .thenError(folly::tag_t<std::exception>{}, [&](const std::exception& e) {
-            LOG(INFO) << e.what();
-            exit(1);
             return folly::makeFuture(client);
         });
 }
@@ -130,7 +91,7 @@ folly::Future<folly::Unit> sendRequests(folly::EventBase* eventBase,
 }
 
 
-void performance_test(folly::EventBase* eventBase) {
+proxygen::HTTPHeaders httpHeaders() {
     proxygen::HTTPHeaders headers;
 
     auto host = fmt::format("{}:{}", FLAGS_target_host, FLAGS_target_port);
@@ -138,35 +99,38 @@ void performance_test(folly::EventBase* eventBase) {
     headers.add(proxygen::HTTP_HEADER_USER_AGENT, "test-client");
     headers.add(proxygen::HTTP_HEADER_ACCEPT, "*/*");
 
+    return headers;
+}
+
+
+std::string makePayload(int size) {
+    std::string payload;
+    for (int i = 0 ; i < FLAGS_payload_size ; i++)
+        payload.push_back('a');
+    return payload;
+}
+
+
+folly::Future< std::vector<HttpClient*> > getClients(folly::EventBase* eventBase, int count) {
+    using namespace std::chrono_literals;
+
     std::vector<HttpClient*> clients;
     std::vector<folly::Future<folly::Unit>> connectFutures;
 
-    auto defaultTimeout = std::chrono::milliseconds(5000);
-
     auto url = fmt::format("http://{}:{}/", FLAGS_target_host, FLAGS_target_port);
 
-    for (int i = 0 ; i < FLAGS_num_connections ; i++) {
-        clients.push_back(new HttpClient(eventBase,
-                                         defaultTimeout,
-                                         headers,
-                                         url));
+    for (int i = 0 ; i < count ; i++) {
+
+        clients.push_back(new HttpClient(eventBase, 5s, httpHeaders(), url));
 
         connectFutures.push_back(clients[i]->connect());
     }
 
-    static std::chrono::time_point<std::chrono::high_resolution_clock> start;
-    static std::string payload;
-
-    payload.clear();
-    for (int i = 0 ; i < FLAGS_payload_size ; i++)
-        payload.push_back('a');
-
-
-    // NOTE: This exits before before any of these events have happened.
-
-    folly::collectAll(std::move(connectFutures))
+    return folly::collectAll(std::move(connectFutures))
         .via(eventBase)
-        .thenValue([eventBase, clients](std::vector<folly::Try<folly::Unit>> results) mutable {
+        .thenValue([eventBase, clients=std::move(clients)]
+                   (std::vector<folly::Try<folly::Unit>> results) mutable {
+
             for (const auto& result : results) {
                 if (! result.hasValue()) {
                     LOG(ERROR) << "Exception: " << result.exception().what();
@@ -176,18 +140,51 @@ void performance_test(folly::EventBase* eventBase) {
 
             LOG(INFO) << "All HttpClient's connected";
 
-            start = std::chrono::high_resolution_clock::now();
-
-            return sendRequests(eventBase, std::move(clients), payload);
-        })
-        .thenValue([clients](folly::Unit) {
-            auto end = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double, std::milli> duration = end - start;
-
-            std::cout << "took " << duration.count() << " ms" << std::endl;
-
-            for (auto& client : clients)
-                delete client;
+            return std::move(clients);
         });
 }
 
+
+void performance_test(folly::EventBase* eventBase) {
+
+    static std::vector<HttpClient*> clients;
+    static std::string payload;
+    static std::chrono::time_point<std::chrono::high_resolution_clock> start;
+
+    getClients(eventBase, FLAGS_num_connections)
+        .thenValue([eventBase](std::vector<HttpClient*> httpClients) {
+
+            clients = httpClients;
+            payload = makePayload(FLAGS_payload_size);
+            start = std::chrono::high_resolution_clock::now();
+
+            return sendRequests(eventBase, clients, payload);
+        })
+        .thenValue([](folly::Unit) {
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::milli> duration = end - start;
+
+            const long ONE_MEG = 0x100000;
+            double seconds = duration.count() / 1000.0;
+            double requestRate = FLAGS_number_of_requests / seconds;
+            double averageRequestTime = duration.count() / FLAGS_number_of_requests;
+            double bitRate = 8 * FLAGS_payload_size * FLAGS_number_of_requests / seconds
+                             / ONE_MEG;
+
+
+            LOG(INFO) << "";
+            LOG(INFO) << "took " << duration.count() / 1000.0 << " seconds"
+                      << " to send " << FLAGS_number_of_requests
+                      << " with " << clients.size() << " connections";
+
+            LOG(INFO) << "request rate     : " << requestRate << " requests/second";
+            LOG(INFO) << "avg request Time : " << averageRequestTime << " ms";
+            LOG(INFO) << "bit rate         : " << bitRate << " Mbps";
+
+            for (auto& client : clients)
+                delete client;
+
+            // HACK: Figure out how to exit immediately after this ends and remove exit() call
+            exit(0);
+        });
+}
