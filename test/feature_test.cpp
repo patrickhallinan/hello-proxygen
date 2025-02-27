48ed73abd58e3decfbe66660e00c6a0510692f89
patrick <patrick.j.hallinan@gmail.com>
2025-02-27 10:34:03 -0500

    wip

--- test/feature_test.cpp
+++ test/feature_test.cpp
@@ -1,30 +1,103 @@
 #include "Test.h"
 
-#include <folly/init/Init.h>
+#include "HttpClient.h"
+
 #include <folly/io/async/EventBase.h>
-#include <gflags/gflags.h>
+#include <fmt/format.h>
+#include <gflags/gflags_declare.h>
+
+#include <cstdlib>
+#include <chrono>
 
 
-DEFINE_string(hello_host, "127.0.0.1", "IP address");
-DEFINE_int32(hello_port, 8080, "HTTP port");
+DECLARE_string(hello_host);
+DECLARE_int32(hello_port);
 
 
-int main(int argc, char* argv[]) {
-    FLAGS_logtostderr = true;
-    FLAGS_minloglevel = 0;
+static proxygen::HTTPHeaders httpHeaders();
 
-    folly::Init _{&argc, &argv, /*removeFlags=*/false};
 
-    folly::EventBase eventBase;
+Test::Test(folly::EventBase& eventBase)
+    : eventBase_{eventBase} {
 
-    Test test(eventBase);
-    test.run();
+    auto defaultTimeout = std::chrono::milliseconds(5000);
 
-    // Test does not run until eventBase.loop()
-    eventBase.loop();
+    auto url = fmt::format("http://{}:{}/", FLAGS_hello_host, FLAGS_hello_port);
 
-    LOG(INFO) << "Exit main()\n";
+    httpClient_ = std::make_unique<HttpClient>(&eventBase_,
+                                               defaultTimeout,
+                                               httpHeaders(),
+                                               url);
+}
 
-    return 0;
+
+void Test::run() {
+    eventBase_.runInEventBaseThread([httpClient=httpClient_.get()]() {
+
+        static bool passed;
+
+        passed = true;
+
+        return httpClient->connect()
+                          .thenValue([httpClient](folly::Unit) {
+                              LOG(INFO) << "Connected!";
+
+                              return httpClient->GET();
+                          })
+                          .thenValue([httpClient](const HttpResponse& response) {
+                              LOG(INFO) << "Status: " << response.status()
+                                  << ", Content: " << response.content();
+
+                              assert_equal(response.status(), 200);
+                              assert_equal(response.content(), "Hello");
+
+                              return httpClient->POST("Echo");
+                          })
+                          .thenValue([httpClient](const HttpResponse& response) {
+                              LOG(INFO) << "Status: " << response.status()
+                                  << ", Content: " << response.content();
+
+                              assert_equal(response.content(), "Echo");
+
+                              return httpClient->POST("");
+                          })
+                          .thenValue([](const HttpResponse& response) {
+                              LOG(INFO) << "Status: " << response.status()
+                                  << ", Content: " << response.content();
+
+                              assert_equal(response.content(), "CONTENT MISSING");
+                          })
+
+                          .thenError(folly::tag_t<std::exception>{}, [](const std::exception& e) {
+                              passed = false;
+                              LOG(INFO) << e.what();
+                          })
+                          .thenValue([httpClient](folly::Unit) {
+                              if (passed) {
+                                  LOG(INFO) << "PASSED";
+                              }
+                              else {
+                                  LOG(INFO) << "FAILED";
+                              }
+
+                              // XXX: Why does this wait 1m 12s after finishing the test?
+
+                              // HACK: Remove once exit delay is figured out.
+                              int status = passed? 0 : 1;
+                              exit(status);
+                          });
+    });
+}
+
+
+proxygen::HTTPHeaders httpHeaders() {
+    proxygen::HTTPHeaders headers;
+
+    auto host = fmt::format("{}:{}", FLAGS_hello_host, FLAGS_hello_port);
+    headers.add(proxygen::HTTP_HEADER_HOST, host);
+    headers.add(proxygen::HTTP_HEADER_USER_AGENT, "test-client");
+    headers.add(proxygen::HTTP_HEADER_ACCEPT, "*/*");
+
+    return headers;
 }
 
---
+++ test/feature_test_main.cpp
@@ -0,0 +1,30 @@
+#include "Test.h"
+
+#include <folly/init/Init.h>
+#include <folly/io/async/EventBase.h>
+#include <gflags/gflags.h>
+
+
+DEFINE_string(hello_host, "127.0.0.1", "IP address");
+DEFINE_int32(hello_port, 8080, "HTTP port");
+
+
+int main(int argc, char* argv[]) {
+    FLAGS_logtostderr = true;
+    FLAGS_minloglevel = 0;
+
+    folly::Init _{&argc, &argv, /*removeFlags=*/false};
+
+    folly::EventBase eventBase;
+
+    Test test(eventBase);
+    test.run();
+
+    // Test does not run until eventBase.loop()
+    eventBase.loop();
+
+    LOG(INFO) << "Exit main()\n";
+
+    return 0;
+}
+
