28be905674c04068730e862805158d2d496da38e
Patrick Hallinan <patrick.j.hallinan@gmail.com>
2025-02-28 16:48:08 -0500

    update install script for rocky linux

--- script/rocky-linux-9-deps.sh
+++ script/rocky-linux-9-deps.sh
@@ -5,4 +5,4 @@ sudo dnf config-manager --set-enabled crb
 sudo dnf install -y cmake gcc gcc-c++ boost boost-devel double-conversion-devel
 sudo dnf install -y fast_float-devel libevent-devel openssl-devel glog-devel zlib-devel libsodium-devel gperf
 sudo dnf install -y libunwind-devel binutils-devel xz-devel lz4-devel snappy-devel libzstd-devel bzip2-devel
-sudo dnf install -y clang
+sudo dnf install -y clang json-devel
